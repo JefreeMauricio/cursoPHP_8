073b71c38bb3322ee103b73d769b0de263a2b55c
JefreeMauricio <jefreegoficial+1@gmail.com>
2026-05-27 12:32:06 -0500

    feat: agregar lógica de cambiar estado completado/pendiente

---
+++ toggle-task.php
@@ -0,0 +1,11 @@
+<?php
+require 'functions.php';
+$query = require 'bootstrap.php';
+
+
+$query->update('tasks',$_POST['id'], [
+    'completed' => $_POST['completed'],
+    
+]);
+
+header('Location: index.php');
